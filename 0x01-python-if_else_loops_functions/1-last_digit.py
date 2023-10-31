e3d688d2abfc88af97124d0c710ec0e4a9e0c5fa
Muhammad Omran <use.yurMind@gmail.com>
2023-10-31 22:09:14 +0200

    1x01-01 fix

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,9 +2,9 @@
 import random
 number = random.randint(-10000, 10000)
 sign = "-" if number < 0 else ""
-if abs(number) % 10 > 5:
+if number % 10 > 5 and sign != "-":
     message = "greater than 5"
-elif abs(number) % 10 == 0:
+elif number % 10 == 0:
     message = "0"
 else:
     message = "less than 6 and not 0"
