1ff405df5acfbc1eb56ca89c46170cd7fb5e06ce
Muhammad Omran <use.yurMind@gmail.com>
2023-10-31 23:51:54 +0200

    1x01-09 this course is bullshit, has double standards

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    sign = -1 if number < 0 else 1
-    print(sign * (abs(number) % 10), end="")
-    return sign * (abs(number) % 10)
+    print((abs(number) % 10), end="")
+    return (abs(number) % 10)
