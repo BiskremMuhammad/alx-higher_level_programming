36c518f5c3b1fcf9074255bf54be22e4d0543d87
Muhammad Omran <use.yurMind@gmail.com>
2023-11-02 17:30:51 +0200

    1x02-02

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,9 @@ import sys
 
 
 def main():
-    print("{} arguments:".format(len(sys.argv) - 1))
+    print("{} {}{}".format(len(sys.argv) - 1,
+          "argument" if len(sys.argv) == 2 else "arguments",
+                           "." if len(sys.argv) == 1 else ":"))
     for i in range(1, len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))
 
