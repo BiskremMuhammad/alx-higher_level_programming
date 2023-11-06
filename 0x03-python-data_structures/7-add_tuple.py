157709f04ae7ce6f15d5f99155d70d09b2286f0d
Muhammad Omran <use.yurMind@gmail.com>
2023-11-06 14:49:41 +0200

    1x03-08

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    return tuple(sum(tuple_a[:2]), sum(tuple_b[:2]), )
+    safe_a = (tuple_a[0] if len(tuple_a) > 0 else 0,
+              tuple_a[1] if len(tuple_a) > 1 else 0)
+    safe_b = (tuple_b[0] if len(tuple_b) > 0 else 0,
+              tuple_b[1] if len(tuple_b) > 1 else 0)
+    new_tuple = (safe_a[0] + safe_b[0], safe_a[1] + safe_b[1])
+    return new_tuple
