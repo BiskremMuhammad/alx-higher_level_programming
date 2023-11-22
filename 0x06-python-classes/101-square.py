0460946b0107e491df6ccfd7c97e9703ba1b57a3
Muhammad Omran <use.yurMind@gmail.com>
2023-11-22 18:19:41 +0200

    1x06-101

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -88,12 +88,3 @@ class Square:
     def area(self):
         """claculate square area"""
         return self.__size ** 2
-
-
-my_square = Square(5, (0, 0))
-print(my_square)
-
-print("--")
-
-my_square = Square(5, (4, 1))
-print(my_square)
