d02df64565435bce2e1aea2a6cff389010980c90
Muhammad Omran <use.yurMind@gmail.com>
2023-11-22 12:59:50 +0200

    1x05-03

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    res = None
+    try:
+        res = a / b
+    except:
+        res = None
+    finally:
+        print("Inside result: {:.1f}".format(res))
