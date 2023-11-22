f9072b22f7e069acb82ff938ce80105be89d9ea6
Muhammad Omran <use.yurMind@gmail.com>
2023-11-22 13:02:11 +0200

    1x05-03

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,4 +6,6 @@ def safe_print_division(a, b):
     except:
         res = None
     finally:
-        print("Inside result: {:.1f}".format(res))
+        print("Inside result: {}".format(res))
+
+    return res
