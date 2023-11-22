1bc8918c8e3f4827410e72c54a17f5fbbcbdd022
Muhammad Omran <use.yurMind@gmail.com>
2023-11-22 16:35:42 +0200

    1x06-05

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+a Square Class definition
+"""
+
+
+class Square:
+    """Square Class"""
+
+    def __init__(self, size=0):
+        """constructor"""
+        self.__size = size
+        if not type(size) == int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+
+    @property
+    def size(self):
+        """retreive size attribute"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """size att setter"""
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def my_print(self):
+        """print a square shape"""
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#")
+            print()
+
+        if self.__size <= 0:
+            print()
+
+    def area(self):
+        """claculate square area"""
+        return self.__size ** 2
