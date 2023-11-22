ba89046556a53b6b1186ff88adb6593cf9a522ab
Muhammad Omran <use.yurMind@gmail.com>
2023-11-22 14:05:13 +0200

    1x06-02

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 class Square:
-    """constructor"""
+    """Defines a square"""
 
     def __init__(self, size=0):
-        """Initialises the size"""
+        """Initialises the data"""
         self.__size = size
-        if not type(size) == int:
-            raise TypeError('size must be an integer')
-        elif size < 0:
-            raise ValueError('size must be >= 0')
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
